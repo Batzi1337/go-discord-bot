70af9dde3826439ba92b91934cb1d867cde10248
Batzi1337 <28926863+Batzi1337@users.noreply.github.com>
2024-09-16 15:14:49 +0200

    start implementing giveaway command

--- cmd/command-bot/main.go
+++ cmd/command-bot/main.go
@@ -5,61 +5,25 @@ import (
 	"go-discord-bot/internal/api/market"
 	"go-discord-bot/internal/config"
 	"go-discord-bot/internal/model"
+	"log"
+	"math/rand"
 	"os"
 	"os/signal"
 	"strings"
 	"sync"
 	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 var (
-	commands = []*discordgo.ApplicationCommand{
-		{
-			Name:        "order",
-			Description: "Place an order in the format WTS/WTB",
-			Options: []*discordgo.ApplicationCommandOption{
-				{
-					Type:        discordgo.ApplicationCommandOptionString,
-					Name:        "type",
-					Description: "Type of order (WTS/WTB)",
-					Required:    true,
-					Choices: []*discordgo.ApplicationCommandOptionChoice{
-						{Name: "WTS", Value: "WTS"},
-						{Name: "WTB", Value: "WTB"},
-					},
-				},
-				{
-					Type:         discordgo.ApplicationCommandOptionString,
-					Name:         "item",
-					Description:  "Item to buy or sell",
-					Required:     true,
-					Autocomplete: true, // Enable autocomplete
-				},
-				{
-					Type:        discordgo.ApplicationCommandOptionInteger,
-					Name:        "price",
-					Description: "Price of the item",
-					Required:    true,
-				},
-			},
-		},
-	}
+	commands           []*discordgo.ApplicationCommand
 	registeredCommands []*discordgo.ApplicationCommand
 )
 
 func main() {
 	run()
-}
-
-func waitForExit() {
-	fmt.Println("Bot is now running. Press CTRL+C to exit.")
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	<-stop
-
-	fmt.Println("Shutting down gracefully...")
 }
 
 func run() {
@@ -95,7 +59,133 @@ func run() {
 		handleAutocomplete(s, i, items)
 	})
 
+	dg.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if i.ApplicationCommandData().Name == "giveaway" {
+			handleGiveawayCommand(s, i)
+		}
+	})
+
 	waitForExit()
+}
+
+func waitForExit() {
+	fmt.Println("Bot is now running. Press CTRL+C to exit.")
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-stop
+
+	fmt.Println("Shutting down gracefully...")
+}
+
+func initializeBot(token string) (*discordgo.Session, error) {
+	dg, err := discordgo.New("Bot " + token)
+	if err != nil {
+		return nil, err
+	}
+
+	err = dg.Open()
+	if err != nil {
+		return nil, err
+	}
+
+	return dg, nil
+}
+
+func registerCommands(dg *discordgo.Session, appID, guildID string) error {
+	commands = []*discordgo.ApplicationCommand{
+		{
+			Name:        "order",
+			Description: "Place an order in the format WTS/WTB",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Type:        discordgo.ApplicationCommandOptionString,
+					Name:        "type",
+					Description: "Type of order (WTS/WTB)",
+					Required:    true,
+					Choices: []*discordgo.ApplicationCommandOptionChoice{
+						{Name: "WTS", Value: "WTS"},
+						{Name: "WTB", Value: "WTB"},
+					},
+				},
+				{
+					Type:         discordgo.ApplicationCommandOptionString,
+					Name:         "item",
+					Description:  "Item to buy or sell",
+					Required:     true,
+					Autocomplete: true, // Enable autocomplete
+				},
+				{
+					Type:        discordgo.ApplicationCommandOptionInteger,
+					Name:        "price",
+					Description: "Price of the item",
+					Required:    true,
+				},
+			},
+		},
+		{
+			Name:        "giveaway",
+			Description: "Start a giveaway",
+		},
+	}
+
+	for _, cmd := range commands {
+		createdCmd, err := dg.ApplicationCommandCreate(appID, guildID, cmd)
+		if err != nil {
+			return err
+		}
+		registeredCommands = append(registeredCommands, createdCmd)
+	}
+	return nil
+}
+
+func fetchItems() ([]*model.Item, error) {
+	items, err := market.FetchItems()
+	if err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
+func handleGiveawayCommand(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	message, err := s.ChannelMessageSend(i.ChannelID, "React with ✅ to enter the giveaway!")
+	if err != nil {
+		fmt.Println("Error sending message: ", err)
+		return
+	}
+
+	err = s.MessageReactionAdd(i.ChannelID, message.ID, "✅")
+	if err != nil {
+		fmt.Println("Error adding reaction: ", err)
+		return
+	}
+
+	users := make(map[string]bool)
+	s.AddHandler(func(s *discordgo.Session, r *discordgo.MessageReactionAdd) {
+		if r.MessageID == message.ID && r.Emoji.Name == "✅" {
+			users[r.UserID] = true
+		}
+	})
+
+	time.AfterFunc(1*time.Hour, func() {
+		if len(users) == 0 {
+			s.ChannelMessageSend(i.ChannelID, "No one entered the giveaway.")
+			return
+		}
+
+		userIDs := make([]string, 0, len(users))
+		for userID := range users {
+			userIDs = append(userIDs, userID)
+		}
+
+		winnerID := userIDs[rand.Intn(len(userIDs))]
+		winner, err := s.User(winnerID)
+		if err != nil {
+			fmt.Println("Error fetching user: ", err)
+			return
+		}
+
+		s.ChannelMessageSend(i.ChannelID, fmt.Sprintf("Congratulations %s! You won the giveaway!", winner.Mention()))
+	})
 }
 
 func handleOrderCommand(s *discordgo.Session, i *discordgo.InteractionCreate, items []*model.Item) {
@@ -128,21 +218,6 @@ func handleOrderCommand(s *discordgo.Session, i *discordgo.InteractionCreate, it
 	if err != nil {
 		logError("Error responding to order command:", err)
 	}
-}
-
-func findItemByName(items []*model.Item, itemName string) (*model.Item, bool) {
-	var item *model.Item
-	for _, it := range items {
-		if it.ItemName == itemName {
-			item = it
-			break
-		}
-	}
-
-	if item == nil {
-		return nil, false
-	}
-	return item, true
 }
 
 func handleAutocomplete(s *discordgo.Session, i *discordgo.InteractionCreate, items []*model.Item) {
@@ -197,43 +272,20 @@ func handleAutocomplete(s *discordgo.Session, i *discordgo.InteractionCreate, it
 	}
 }
 
-func fetchItems() ([]*model.Item, error) {
-	items, err := market.FetchItems()
-	if err != nil {
-		return nil, err
+func findItemByName(items []*model.Item, itemName string) (*model.Item, bool) {
+	for _, it := range items {
+		if it.ItemName == itemName {
+			return it, true
+		}
 	}
-	return items, nil
+
+	return nil, false
 }
 
 func logError(message string, err error) {
-	fmt.Println(message, err)
+	log.Println(message, err)
 }
 
 func logInfo(message string) {
-	fmt.Println(message)
-}
-
-func initializeBot(token string) (*discordgo.Session, error) {
-	dg, err := discordgo.New("Bot " + token)
-	if err != nil {
-		return nil, err
-	}
-
-	err = dg.Open()
-	if err != nil {
-		return nil, err
-	}
-
-	return dg, nil
-}
-
-func registerCommands(dg *discordgo.Session, appID, guildID string) error {
-	for _, cmd := range commands {
-		createdCmd, err := dg.ApplicationCommandCreate(appID, guildID, cmd)
-		if err != nil {
-			return err
-		}
-		registeredCommands = append(registeredCommands, createdCmd)
-	}
-	return nil
+	log.Println(message)
 }
